dbbf289c8c348bc61525149ed0e56ee47146edc7
Kaiden Ashun <kaiden.ashun@outlook.com>
2018-03-26 13:04:20 +0000

    Added the route for index

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def index():
     """Landing Page for users"""
-    
+    return render_template("index.html", page_title="Home page")
     
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug = True)
