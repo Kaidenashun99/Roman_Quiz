9a56f57d8f1d31f271a1f8c9642d9be744b12272
Kaiden Ashun <kaiden.ashun@outlook.com>
2018-03-28 15:52:29 +0000

    Added basic functionality user is redirected to quiz page upon entering their chosen username

--- run.py
+++ run.py
@@ -3,10 +3,27 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+""" Writes data to selected file """ 
+def write_to_file(filename, data):
+    """Handle the process of writing data to a file"""
+    with open(filename, "a") as file:
+        file.writelines(data)
+
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-    """Landing Page for users"""
+    """Landing Page for users"""""
+    if request.method == "POST":
+        with open("data/users.txt", "a") as user_list:
+                user_list.write(request.form["username"] + "\n")
+                return redirect("quiz")
     return render_template("index.html", page_title="Home page")
+    
+
+
+@app.route('/quiz')
+def quiz():
+    return render_template("quiz.html")
     
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug = True)
