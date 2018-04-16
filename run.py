ed32fe5c3eaad986e4fba9c02cc90ac11adb2020
Kaiden Ashun <kaiden.ashun@outlook.com>
2018-04-16 11:47:14 +0000

    Updated files

--- run.py
+++ run.py
@@ -20,6 +20,7 @@ def index():
     """Home Page for users"""
     if request.method == "POST":
             flash("Thanks, you selected {} as your username".format(request.form["username"]))
+            return redirect("/quiz")
     return render_template("index.html", page_title="Home page")
     
 
@@ -27,7 +28,15 @@ def index():
 def quiz():
     with open('data/questions.json') as json_data:
         data = json.load(json_data)
+        
+    score = 0
+    
+    if request.method == "POST":
+        if (request.form["answer"]) == "answer1":
+            score +=1
+            flash("Your current score is {}".format(score))
     return render_template("quiz.html", page_title="Quiz", quiz_data=data)
+    
     
    
     
